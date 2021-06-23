0b1888e88777167db716468573c903fb9ccd7033
jocelynrem <jocelynrem@gmail.com>
2021-06-22 23:26:06 -0400

    connects to server

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 // get one product
 // find a single product by its `id`
 // be sure to include its associated Category and Tag data
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Tag }, { model: Category }]
